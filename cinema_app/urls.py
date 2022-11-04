cf4d87b701c37b4eb3149e3f884d40d49b881278
DanRoman-code <coderc770@gmail.com>
2022-11-04 12:03:01 +0200

    fixed url to movie_id

--- cinema_app/urls.py
+++ cinema_app/urls.py
@@ -5,7 +5,7 @@ from cinema_app.views import GenresListView, MoviesListView, MoviesDetailView
 urlpatterns = [
     path("genres/", GenresListView.as_view(), name="genres-list"),
     path("movies/", MoviesListView.as_view(), name="movies-list"),
-    path("movies/<int:pk>", MoviesDetailView.as_view(), name="movie-detail"),
+    path("movies/<int:pk>/", MoviesDetailView.as_view(), name="movie-detail"),
 ]
 
 app_name = "cinema"
